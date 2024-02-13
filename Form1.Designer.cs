9a00eb4ead4ee30c758426085d81a16c2f73bce6
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2024-02-13 18:50:17 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_sete = new Button();
+            btn_oito = new Button();
+            btn_nove = new Button();
+            btn_adicao = new Button();
+            btn_quatro = new Button();
+            btn_cinco = new Button();
+            btn_ponto = new Button();
+            btn_um = new Button();
+            btn_dois = new Button();
+            btn_tres = new Button();
+            btn_enter = new Button();
+            btn_zero = new Button();
+            btn_virgula = new Button();
+            btn_seis = new Button();
+            btn_del = new Button();
+            btn_divisao = new Button();
+            btn_multiplicacao = new Button();
+            btn_subtracao = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // btn_sete
+            // 
+            btn_sete.Location = new Point(8, 143);
+            btn_sete.Name = "btn_sete";
+            btn_sete.Size = new Size(80, 50);
+            btn_sete.TabIndex = 0;
+            btn_sete.Text = "7";
+            btn_sete.UseVisualStyleBackColor = true;
+            // 
+            // btn_oito
+            // 
+            btn_oito.Location = new Point(96, 143);
+            btn_oito.Name = "btn_oito";
+            btn_oito.Size = new Size(80, 50);
+            btn_oito.TabIndex = 1;
+            btn_oito.Text = "8";
+            btn_oito.UseVisualStyleBackColor = true;
+            // 
+            // btn_nove
+            // 
+            btn_nove.Location = new Point(180, 143);
+            btn_nove.Name = "btn_nove";
+            btn_nove.Size = new Size(80, 50);
+            btn_nove.TabIndex = 2;
+            btn_nove.Text = "9";
+            btn_nove.UseVisualStyleBackColor = true;
+            // 
+            // btn_adicao
+            // 
+            btn_adicao.Location = new Point(266, 143);
+            btn_adicao.Name = "btn_adicao";
+            btn_adicao.Size = new Size(80, 50);
+            btn_adicao.TabIndex = 3;
+            btn_adicao.Text = "+";
+            btn_adicao.UseVisualStyleBackColor = true;
+            // 
+            // btn_quatro
+            // 
+            btn_quatro.Location = new Point(8, 199);
+            btn_quatro.Name = "btn_quatro";
+            btn_quatro.Size = new Size(80, 50);
+            btn_quatro.TabIndex = 4;
+            btn_quatro.Text = "4";
+            btn_quatro.UseVisualStyleBackColor = true;
+            // 
+            // btn_cinco
+            // 
+            btn_cinco.Location = new Point(94, 199);
+            btn_cinco.Name = "btn_cinco";
+            btn_cinco.Size = new Size(80, 50);
+            btn_cinco.TabIndex = 5;
+            btn_cinco.Text = "5";
+            btn_cinco.UseVisualStyleBackColor = true;
+            // 
+            // btn_ponto
+            // 
+            btn_ponto.Location = new Point(266, 199);
+            btn_ponto.Name = "btn_ponto";
+            btn_ponto.Size = new Size(80, 50);
+            btn_ponto.TabIndex = 7;
+            btn_ponto.Text = ".";
+            btn_ponto.UseVisualStyleBackColor = true;
+            // 
+            // btn_um
+            // 
+            btn_um.Location = new Point(8, 255);
+            btn_um.Name = "btn_um";
+            btn_um.Size = new Size(80, 50);
+            btn_um.TabIndex = 8;
+            btn_um.Text = "1";
+            btn_um.UseVisualStyleBackColor = true;
+            // 
+            // btn_dois
+            // 
+            btn_dois.Location = new Point(96, 255);
+            btn_dois.Name = "btn_dois";
+            btn_dois.Size = new Size(80, 50);
+            btn_dois.TabIndex = 9;
+            btn_dois.Text = "2";
+            btn_dois.UseVisualStyleBackColor = true;
+            // 
+            // btn_tres
+            // 
+            btn_tres.Location = new Point(180, 255);
+            btn_tres.Name = "btn_tres";
+            btn_tres.Size = new Size(80, 50);
+            btn_tres.TabIndex = 10;
+            btn_tres.Text = "3";
+            btn_tres.UseVisualStyleBackColor = true;
+            // 
+            // btn_enter
+            // 
+            btn_enter.Location = new Point(266, 255);
+            btn_enter.Name = "btn_enter";
+            btn_enter.Size = new Size(80, 106);
+            btn_enter.TabIndex = 11;
+            btn_enter.Text = "Enter";
+            btn_enter.UseVisualStyleBackColor = true;
+            // 
+            // btn_zero
+            // 
+            btn_zero.Location = new Point(8, 311);
+            btn_zero.Name = "btn_zero";
+            btn_zero.Size = new Size(168, 50);
+            btn_zero.TabIndex = 12;
+            btn_zero.Text = "0";
+            btn_zero.UseVisualStyleBackColor = true;
+            // 
+            // btn_virgula
+            // 
+            btn_virgula.Location = new Point(180, 311);
+            btn_virgula.Name = "btn_virgula";
+            btn_virgula.Size = new Size(80, 50);
+            btn_virgula.TabIndex = 13;
+            btn_virgula.Text = ",";
+            btn_virgula.UseVisualStyleBackColor = true;
+            // 
+            // btn_seis
+            // 
+            btn_seis.Location = new Point(180, 199);
+            btn_seis.Name = "btn_seis";
+            btn_seis.Size = new Size(80, 50);
+            btn_seis.TabIndex = 6;
+            btn_seis.Text = "6";
+            btn_seis.UseVisualStyleBackColor = true;
+            // 
+            // btn_del
+            // 
+            btn_del.Location = new Point(8, 87);
+            btn_del.Name = "btn_del";
+            btn_del.Size = new Size(80, 50);
+            btn_del.TabIndex = 14;
+            btn_del.Text = "DEL";
+            btn_del.UseVisualStyleBackColor = true;
+            // 
+            // btn_divisao
+            // 
+            btn_divisao.Location = new Point(94, 87);
+            btn_divisao.Name = "btn_divisao";
+            btn_divisao.Size = new Size(80, 50);
+            btn_divisao.TabIndex = 15;
+            btn_divisao.Text = "/";
+            btn_divisao.UseVisualStyleBackColor = true;
+            // 
+            // btn_multiplicacao
+            // 
+            btn_multiplicacao.Location = new Point(180, 87);
+            btn_multiplicacao.Name = "btn_multiplicacao";
+            btn_multiplicacao.Size = new Size(80, 50);
+            btn_multiplicacao.TabIndex = 16;
+            btn_multiplicacao.Text = "*";
+            btn_multiplicacao.UseVisualStyleBackColor = true;
+            // 
+            // btn_subtracao
+            // 
+            btn_subtracao.Location = new Point(266, 87);
+            btn_subtracao.Name = "btn_subtracao";
+            btn_subtracao.Size = new Size(80, 50);
+            btn_subtracao.TabIndex = 17;
+            btn_subtracao.Text = "-";
+            btn_subtracao.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(8, 36);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(340, 45);
+            textBox1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(366, 380);
+            Controls.Add(textBox1);
+            Controls.Add(btn_subtracao);
+            Controls.Add(btn_multiplicacao);
+            Controls.Add(btn_divisao);
+            Controls.Add(btn_del);
+            Controls.Add(btn_virgula);
+            Controls.Add(btn_zero);
+            Controls.Add(btn_enter);
+            Controls.Add(btn_tres);
+            Controls.Add(btn_dois);
+            Controls.Add(btn_um);
+            Controls.Add(btn_ponto);
+            Controls.Add(btn_seis);
+            Controls.Add(btn_cinco);
+            Controls.Add(btn_quatro);
+            Controls.Add(btn_adicao);
+            Controls.Add(btn_nove);
+            Controls.Add(btn_oito);
+            Controls.Add(btn_sete);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculadora do RHC";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn_sete;
+        private Button btn_oito;
+        private Button btn_nove;
+        private Button btn_adicao;
+        private Button btn_quatro;
+        private Button btn_cinco;
+        private Button btn_ponto;
+        private Button btn_um;
+        private Button btn_dois;
+        private Button btn_tres;
+        private Button btn_enter;
+        private Button btn_zero;
+        private Button btn_virgula;
+        private Button btn_seis;
+        private Button btn_del;
+        private Button btn_divisao;
+        private Button btn_multiplicacao;
+        private Button btn_subtracao;
+        private TextBox textBox1;
+    }
+}
